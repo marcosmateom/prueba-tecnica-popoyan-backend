5c29fc86e30d4f2e3457e8c97f991eeb0ee45136
marcosmateom <marcosmateom@gmail.com>
2025-05-20 20:59:58 -0600

    install Apollo and setup app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { User } from './user/user.entity';
+import { Comment } from './comment/comment.entity';
+import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nestbackend',
+      password: 'MJDG7KyoJrpAaxnM',
+      database: 'popoyandb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+    }),
+    TypeOrmModule.forFeature([User, Comment]),
+    UserModule,
+    CommentModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
